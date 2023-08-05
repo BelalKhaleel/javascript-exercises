52a749957603ef026a7c84730dbf7637fa7dce9e
Belal Khaleel <119034702+BelalKhaleel@users.noreply.github.com>
2023-08-05 18:54:42 +0300

    Finish exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,23 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  if (num < 0) {
+    return 'ERROR';
+  } else {
+  let arr = [];
+  for (let i = 0; i < num; i++) {
+    arr.push(string);
+  }
+  return arr.join("");
+}
 };
 
+const number = Math.floor(Math.random() * 1000);
+
+repeatString('hey', 3);
+repeatString('hey', 10);
+repeatString('hey', 1);
+repeatString('hey', 0);
+repeatString('hey', -1);
+repeatString('hey', number);
+repeatString('', 10)
 // Do not edit below this line
 module.exports = repeatString;
