a97e095b26ab706e57dbc472821bb6ab22b30773
Belal Khaleel <119034702+BelalKhaleel@users.noreply.github.com>
2023-08-05 21:33:05 +0300

    Pass first 2 tests of exercise 4 successfully

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, arg1, arg2, ...args) {
+  arr.splice(arg1 - 1, 1)
+  if (arg2) {
+    arr.splice(arg2 - 1, 1);
+  }
+  return arr;
 };
 
+removeFromArray([1, 2, 3, 4], 3);
+removeFromArray([1, 2, 3, 4], 3, 2);
+removeFromArray([1, 2, 3, 4], 7, "tacos");
 // Do not edit below this line
 module.exports = removeFromArray;
