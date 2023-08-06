481c4a5732daaf0c7ee5aae698bbb4dffaad8afc
Belal Khaleel <119034702+BelalKhaleel@users.noreply.github.com>
2023-08-06 12:10:29 +0300

    Pass first 2 tests in sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+  
+  let arr = [];
+  for (let i = int1; i <= int2; i++) {
+    arr.push(i);
+  }
+  let sum = 0;
+  for (let j = 0; j < arr.length; j++) {
+    sum += arr[j];
+  }
+  return sum;
 };
 
+sumAll(1, 4);
 // Do not edit below this line
 module.exports = sumAll;
