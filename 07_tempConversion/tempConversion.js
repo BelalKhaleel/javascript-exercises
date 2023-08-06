231c2b08163ee27e46f07d25404c506f30f9ba2b
Belal Khaleel <119034702+BelalKhaleel@users.noreply.github.com>
2023-08-06 14:21:42 +0300

    Finish tempConversion exercise successfully

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+  function round(value, precision) {
+    let multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+}
+  tempInCelsius = (tempInFahrenheit - 32) * 5/9;
+  return round(tempInCelsius, 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsius) {
+  function round(value, precision) {
+    let multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+}
+  tempInFahrenheit = (tempInCelsius * 9/5) + 32;
+  return round(tempInFahrenheit, 1);
 };
 
+convertToCelsius(-100);
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
