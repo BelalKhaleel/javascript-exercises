392a722b3ae24a63a581722acdaa0403b056ebcd
Belal Khaleel <119034702+BelalKhaleel@users.noreply.github.com>
2023-08-06 12:29:50 +0300

    Finish sumAll exercise successfully

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,18 @@
 const sumAll = function(int1, int2) {
-  
+  debugger
   let arr = [];
+
+  if (int1 < 0 || int2 < 0 || !(typeof int1 === "number") || !(typeof int2 === "number")) {
+    return "ERROR";
+
+  } else if (int1 < int2) {
   for (let i = int1; i <= int2; i++) {
     arr.push(i);
+  }
+  } else if (int1 > int2) {
+  for (let i = int2; i <= int1; i++) {
+    arr.push(i);
+  }
   }
   let sum = 0;
   for (let j = 0; j < arr.length; j++) {
@@ -11,6 +21,6 @@ const sumAll = function(int1, int2) {
   return sum;
 };
 
-sumAll(1, 4);
+sumAll(-10, 4)
 // Do not edit below this line
 module.exports = sumAll;
