448acdc9b98eb84ea9eb6329db0faced2097910f
Belal Khaleel <119034702+BelalKhaleel@users.noreply.github.com>
2023-09-02 17:26:16 +0300

    Finish calculator and palindromes exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, current) => product * current);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  debugger
+  if (num === 0) {
+    return 1;
+  } else {
+	let arr = [];
+  for (let i = num; i > 0; i--) {
+    arr.push(i);
+  }
+  return arr.reduce((product, current) => product * current);
+}
 };
 
 // Do not edit below this line
